aee1c5330a6a9fc31ca1cb810cebeca554111587
Girish G Gonda <girishggonda@Girishs-MacBook-Pro.local>
2024-11-02 08:57:13 +0530

    integrated with frontend

--- src/main/java/com/ecommerce/cart/controller/CartController.java
+++ src/main/java/com/ecommerce/cart/controller/CartController.java
@@ -1,12 +1,15 @@
 package com.ecommerce.cart.controller;
 
 import com.ecommerce.cart.model.dto.CartItemDto;
+import com.ecommerce.cart.model.entity.CartItem;
 import com.ecommerce.cart.request.QuantityReq;
 import com.ecommerce.cart.response.CartResponse;
 import com.ecommerce.cart.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/cart")
@@ -40,5 +43,24 @@ public class CartController {
         } else {
             cartService.removeFromCart(userId, productId);
         }
+    }
+
+
+    @GetMapping("/items/{userId}")
+    public ResponseEntity<List<CartItem>> getCartItems(@PathVariable Long userId) {
+        List<CartItem> cartItems = cartService.cartItemofUser(userId);
+        return ResponseEntity.ok(cartItems);
+    }
+
+    @GetMapping("/id/{userId}")
+    public ResponseEntity<Long> getCartId(@PathVariable Long userId) {
+        Long cartId = cartService.getCartId(userId);
+        return ResponseEntity.ok(cartId);
+    }
+
+    @DeleteMapping("/delete/{cartId}")
+    public ResponseEntity<Void> deleteCart(@PathVariable Long cartId) {
+        cartService.deleteCart(cartId);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/ecommerce/cart/repository/CartItemRepository.java
+++ src/main/java/com/ecommerce/cart/repository/CartItemRepository.java
@@ -2,6 +2,8 @@ package com.ecommerce.cart.repository;
 
 import com.ecommerce.cart.model.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 }
